c0545ee77c89cd206cd4772e7cb9305e6d6bde35
Walter Walden <Waldman200036@gmail.com>
2022-10-04 18:59:31 -0500

    Fetch Error Handling through new Router

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useRouteError } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
@@ -17,3 +18,23 @@ function ErrorPage() {
 }
 
 export default ErrorPage;
+=======
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+
+function ErrorPage(){
+const error = useRouteError();
+
+  return (
+    <>
+    <MainNavigation/>
+    <main id="error-content">
+      <h1>An error occured!</h1>
+      <p>{error.message}</p>
+    </main>
+    </>
+  )
+}
+
+export default ErrorPage;
+>>>>>>> Fetch Error Handling through new Router
--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,24 +1,42 @@
 import { useLoaderData } from 'react-router-dom';
+<<<<<<< HEAD
 
+=======
+>>>>>>> Fetch Error Handling through new Router
 import BlogPost from '../components/BlogPost';
 import NewsletterSignup from '../components/NewsletterSignup';
 import { getPost } from '../util/api';
 
 function PostDetailPage() {
+<<<<<<< HEAD
   const postData = useLoaderData();
 
   return (
     <>
       <BlogPost title={postData.title} text={postData.body} />
       <NewsletterSignup />
+=======
+ const postData = useLoaderData();
+  return (
+    <>
+
+      <BlogPost title={postData.title} text={postData.body} />
+>>>>>>> Fetch Error Handling through new Router
     </>
   );
 }
 
 export default PostDetailPage;
 
+<<<<<<< HEAD
 export function loader({ params }) {
   const postId = params.id;
 
   return getPost(postId);
 }
+=======
+export function loader({params}){
+  const postId = params.id;
+  return getPost(postId);
+}
+>>>>>>> Fetch Error Handling through new Router
